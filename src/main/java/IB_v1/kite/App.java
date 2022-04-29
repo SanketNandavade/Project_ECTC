4b0a934aab3cad81ef3dafdbb2bd2f12891cf5f8
Sanket <Sanket@Sanket>
2022-04-29 20:09:22 +0530

    upload

---
+++ src/main/java/IB_v1/kite/App.java
@@ -0,0 +1,13 @@
+package IB_v1.kite;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
